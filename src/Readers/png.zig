634f3e77be2e1c117b85d6eadb0e86f1ed9c90b6
IdiotAtWork <bphilip777@gmail.com>
2026-02-20 18:18:20 -0500

    Added unit test for checking if chunk types works properly

--- src/Readers/png.zig
+++ src/Readers/png.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 const DecodeError = @import("Error.zig").DecodeError;
 const isSigSame = @import("Misc.zig").isSigSame;
 
@@ -7,8 +8,19 @@ pub fn readPng(r: *std.Io.Reader) !void {
     const exp_sig: []const u8 = &.{ 137, 80, 78, 71, 13, 10, 26, 10 };
     try isSigSame(sig, exp_sig);
 
-    const chunk = try Chunk.read(r);
-    std.debug.print("{f}\n", .{chunk});
+    const hdr = try Chunk.read(r);
+    std.debug.print("{f}\n", .{hdr});
+    try r.discardAll(hdr.len);
+    try discardCrc(r);
+
+    while (true) {
+        const chunk = try Chunk.read(r);
+        std.debug.print("{f}\n", .{chunk});
+        try r.discardAll(chunk.len);
+        try discardCrc(r);
+
+        if (chunk.type == .IEND) break;
+    }
 }
 
 const Chunk = struct {
@@ -17,18 +29,17 @@ const Chunk = struct {
 
     pub fn read(r: *std.Io.Reader) !@This() {
         const chunk_len = try r.takeInt(u32, .big);
-        const chunk_type_str = try r.takeArray(4);
+        const chunk_type_str = try r.take(4);
 
         const ChunkTypeEnums = @typeInfo(ChunkTypes).@"union".tag_type.?;
         std.debug.print("Chunk Type Str: {s}\n", .{chunk_type_str});
 
-        const chunk_type = std.meta.stringToEnum(ChunkTypeEnums, chunk_type_str) orelse
-            return DecodeError.InvalidChunkType;
+        const chunk_type = std.meta.stringToEnum(ChunkTypeEnums, chunk_type_str) orelse .unknown;
 
         return .{
             .len = chunk_len,
             .type = switch (chunk_type) {
-                .unknown => .{ .unknown = chunk_type_str },
+                .unknown => .{ .unknown = chunk_type_str[0..4].* },
                 inline else => |t| t,
             },
         };
@@ -43,12 +54,50 @@ const Chunk = struct {
 };
 
 const ChunkTypes = union(enum) {
-    unknown: []const u8,
+    unknown: [4]u8,
     IHDR,
+    // sRGB
+    // gAMA
+    // pHYS,
+    // iTXt
     // IDAT,
     IEND,
 };
 
 fn discardCrc(r: *std.Io.Reader) !void {
     return r.discardAll(4);
+}
+
+test "Parse Chunks" {
+    const filepath: []const u8 = "src/Data/BasicArt.png";
+    if (filepath.len == 0) return error.InvalidFilepath;
+
+    const file = try std.fs.cwd().openFile(filepath, .{ .mode = .read_only });
+    defer file.close();
+
+    var read_buffer: [4096]u8 = undefined;
+    var reader = file.reader(&read_buffer);
+
+    const expected_chunk_types = [_]ChunkTypes{
+        .IHDR,
+        .{ .unknown = "sRGB" },
+        .{ .unknown = "gAMA" },
+        .{ .unknown = "pHYs" },
+        .{ .unknown = "iTXt" },
+        .{ .unknown = "iDAT" },
+        .IEND,
+    };
+
+    for (0..expected_chunk_types.len - 1) |i| {
+        const chunk = try Chunk.read(reader);
+        try reader.discardAll(chunk.len);
+        try discardCrc(reader);
+        try testing.expect(expected_chunk_types[i] == chunk.type);
+        switch (chunk.type) {
+            .unknown => |data| try testing.expectEqualStrings(expected_chunk_types.unknown, data),
+            else => {},
+        }
+    }
+    const chunk = try Chunk.read(reader);
+    try testing.expect(expected_chunk_types[expected_chunk_types.len - 1] == chunk.type);
 }
