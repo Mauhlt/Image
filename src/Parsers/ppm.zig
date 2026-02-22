8c8b62f750ad78ac50c55c1be82bb91cdb1c278a
IdiotAtWork <bphilip777@gmail.com>
2026-02-22 00:28:04 -0500

    Added writer test to ppm

--- src/Parsers/ppm.zig
+++ src/Parsers/ppm.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Image = @import("Image.zig").Image2D;
 const isSigSame = @import("Misc.zig").isSigSame;
+const testing = std.testing;
 
 const PPM = @This();
 
@@ -40,4 +41,34 @@ pub fn write(w: *std.Io.Writer, img: Image) !void {
     try w.write(img.width);
     try w.write(img.height);
     try w.writeAll(img.data);
+}
+
+test "PPM Writer" {
+    var f1 = try std.fs.cwd().openFile("Data/BasicArt.ppm", .{});
+    defer f1.close();
+
+    var f2 = try std.fs.cwd().openFile("Data/NewArt.ppm", .{});
+    defer f2.close();
+
+    const stat1 = try f1.stat();
+    const stat2 = try f2.stat();
+    if (stat1.size != stat2.size) return error.MismatchFileLengths;
+
+    var buf1: [1 * 1024 * 1024]u8 = undefined;
+    var buf2: [1 * 1024 * 1024]u8 = undefined;
+
+    var reader1 = f1.reader(&buf1);
+    var reader2 = f2.reader(&buf2);
+
+    var rbuf1: [1024]u8 = undefined;
+    var rbuf2: [1024]u8 = undefined;
+
+    var i: usize = 0;
+    while (i < stat1.size) {
+        const len1 = try reader1.interface.readSliceShort(&rbuf1);
+        const len2 = try reader2.interface.readSliceShort(&rbuf2);
+        testing.expect(len1 == len2);
+        testing.expectEqual(rbuf1[0..len1], rbuf2[0..len2]);
+        i += len1;
+    }
 }
