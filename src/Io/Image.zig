e949194c7a27f3037f7b5935e4fba6adbab99e21
IdiotAtWork <bphilip777@gmail.com>
2026-03-07 21:24:25 -0500

    Added more features to image

--- src/Io/Image.zig
+++ src/Io/Image.zig
@@ -4,37 +4,53 @@ const testing = std.testing;
 /// Goal:
 /// 1. Create an intrusive interface to load images
 /// 2. Types of supported images:
-///    - width, height, depth
+///    - width, height
 ///    - u8
+///    - rgb, rgba
+///    - srgb, linear
 ///    - variable image data + variable images
 /// 3. TODO:
 ///     - depth
 ///     - u16, i8, i16, f16, f32, f64
 ///     - const image data + const images
-pub fn ColorStruct(comptime T: type) type {
-    return struct {
-        r: T,
-        g: T,
-        b: T,
-        a: T,
+pub fn ColorStruct(comptime bit_type: BitType) type {
+    return switch (bit_type) {
+        .rgb => struct { r: u8, g: u8, b: u8 },
+        .rgba => struct { r: u8, g: u8, b: u8, a: u8 },
     };
 }
 
-const BitTypes = enum {
-    // rgb,
+const BitType = enum(u8) {
+    rgb,
     rgba,
+};
+
+const Colorspace = enum(u8) {
+    srgb = 0,
+    linear = 1,
 };
 
 /// Image Struct:
 /// T: must be an integer or float
-pub fn ImageStruct() type {
-    return struct {
-        width: u32,
-        height: u32,
-        data: []RGBA,
+pub fn ImageStruct(comptime bit_type: BitType) type {
+    return switch (bit_type) {
+        .rgb => struct {
+            width: u32 = 0,
+            height: u32 = 0,
+            colorspace: Colorspace = .srgb,
+            data: []RGB = undefined,
+        },
+        .rgba => struct {
+            width: u32 = 0,
+            height: u32 = 0,
+            colorspace: Colorspace = .srgb,
+            data: []RGBA = undefined,
+        },
     };
 }
 
 /// Common Structs
-pub const RGBA = ColorStruct(u8);
-pub const Image2D = ImageStruct(false);
+pub const RGB = ColorStruct(.rgb);
+pub const RGBA = ColorStruct(.rgba);
+pub const Image2DRGB = ImageStruct(.rgb);
+pub const Image2DRGBA = ImageStruct(.rgba);
--- src/Io/qoi.zig
+++ src/Io/qoi.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const isSigSame = @import("Misc.zig").isSigSame;
+const RGB = @import("Image.zig").RGB;
+const RGBA = @import("Image.zig").RGBA;
 const Image = @import("Image.zig").Image2D;
 
 hdr: Header,
@@ -57,22 +59,59 @@ const Header = struct {
         try w.writeInt(u8, @intFromEnum(self.colorspace), .big);
     }
 
-    pub fn format(self: *const @This(), w: *std.Io.Writer) void {}
+    pub fn format(self: *const @This(), w: *std.Io.Writer) !void {
+        try w.print("{any}\n", .{self.*});
+    }
 };
 
 const Body = struct {
-    pub fn read(self: *const @This(), r: *std.Io.Reader, hdr: *const Header) !@This() {
+    pub fn readRGBA(self: *const @This(), r: *std.Io.Reader, hdr: *const Header) !@This() {
         _ = self;
         _ = r;
         _ = hdr;
+        // array of previous seen pixels, color channels = assumed to be un-premultiplied alpha
+        var previous_pixel: RGBA = .{ .r = 0, .g = 0, .b = 0, .a = 255 };
+        var running_array: [64]RGBA = [_]RGBA{ .r = 0, .g = 0, .b = 0, .a = 0 } ** 64;
+        // run of previous pixel,
+        // index into array of previously seen pixels,
+        // a diff to previous pixel value in rgb
+        // full rgb or rgba values
     }
 
-    pub fn write(self: *const QOI, w: *std.Io.Writer) !void {
+    pub fn readRGB(self: *const @This(), r: *std.Io.Reader, hdr: *const Header) !@This() {
+        _ = self;
+        _ = r;
+        _ = hdr;
+        var previous_pixel: RGB = .{ .r = 0, .g = 0, .b = 0 };
+        var running_array: [64]RGB = [_]RGB{.{ .r = 0, .g = 0, .b = 0 }} ** 64;
+    }
+
+    pub fn write(self: *const @This(), w: *std.Io.Writer) !void {
         _ = self;
         _ = w;
     }
 
     pub fn format(self: *const @This(), w: *std.Io.Writer) !void {
-        w.print("", .{});
+        try w.print("{}\n", .{self.*});
     }
+};
+
+fn hash(c: RGBA) u6 {
+    return @truncate(c.r *% 3 +% c.g *% 5 +% c.b *% 7 +% c.a *% 11);
+}
+
+const Bit64Tags = enum(u64) {
+    end = 0b0100000000000000,
+};
+
+const Bit8Tags = enum(u64) {
+    rgb = 0b11111110,
+    rgba = 0b11111111,
+};
+
+const Bit2Tags = enum(u8) {
+    index = 0b00,
+    diff = 0b01,
+    luma = 0b10,
+    run = 0b11,
 };
