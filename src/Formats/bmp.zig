28fcc81d7334b4f26cfbcf3b18215c13a49656a9
IdiotAtWork <bphilip777@gmail.com>
2026-03-09 11:14:33 -0400

    Converted comptime int to usize

--- src/Formats/bmp.zig
+++ src/Formats/bmp.zig
@@ -14,7 +14,7 @@ const isSigSame = @import("Misc.zig").isSigSame;
 pub fn read(r: *std.Io.Reader, gpa: std.mem.Allocator) !Image {
     const hdr: Header = try .read(r, gpa);
     std.debug.print("{} - {}\n", .{ hdr.width, hdr.height });
-    const pixel_len = switch (hdr.bits_per_pixel) {
+    const pixel_len: usize = switch (hdr.bits_per_pixel) {
         .rgba => 4,
         else => 3,
     };
