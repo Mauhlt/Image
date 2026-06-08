5908967127f75a5f1f552b78985f862c11f66f15
IdiotAtWork <bphilip777@gmail.com>
2026-06-08 13:02:43 -0400

    Made superstructure toPixel to abstract conversion in simple and performant manner

--- src/Formats/color.zig
+++ src/Formats/color.zig
@@ -37,6 +37,177 @@ const PixelOrder = enum(u64) {
     rgba,
 };
 
+inline fn isValidDataLen(data: []const u8, step: usize) bool {
+    return @mod(data.len, step) == 0;
+}
+
+inline fn rgbaFromDataOrder(data_order: DataOrder) !RGBA {
+    switch (data_order) {
+        .rgba,
+        .rbga,
+        .grba,
+        .gbra,
+        .brga,
+        .bgra,
+        .rgb,
+        .rbg,
+        .grb,
+        .gbr,
+        .brg,
+        .bgr,
+        => |tag| {
+            const tagname = @tagName(tag);
+            return .{
+                .r = @truncate(std.mem.indexOfScalar(u8, tagname, 'r').?),
+                .g = @truncate(std.mem.indexOfScalar(u8, tagname, 'g').?),
+                .b = @truncate(std.mem.indexOfScalar(u8, tagname, 'b').?),
+                .a = @truncate(std.mem.indexOfScalar(u8, tagname, 'a') orelse 3),
+            };
+        },
+        else => return error.InvalidDataOrder,
+        _ => return error.UnsupportedDataOrder,
+    }
+}
+
+fn computeStride(data_order: DataOrder) !usize {
+    return switch (data_order) {
+        .gray => 1,
+        .rgb, .rbg, .grb, .gbr, .brg, .bgr => 3,
+        .rgba, .rbga, .grba, .gbra, .brga, .bgra => 4,
+        _ => return error.UnsupportedDataOrder,
+    };
+}
+
+inline fn grayFromRGB(rgb: RGB) GRAY {
+    return @intFromFloat(0.299 * @as(f32, @floatFromInt(rgb.r)) + //
+        0.587 * @as(f32, @floatFromInt(rgb.g)) + //
+        0.114 * @as(f32, @floatFromInt(rgb.b)));
+}
+
+inline fn rgbFromGray(gray: GRAY) RGB {
+    return .{
+        .r = @intFromFloat(0.299 * @as(f32, @floatFromInt(gray))),
+        .g = @intFromFloat(0.587 * @as(f32, @floatFromInt(gray))),
+        .b = @intFromFloat(0.114 * @as(f32, @floatFromInt(gray))),
+    };
+}
+
+inline fn rgbaFromGray(gray: GRAY) RGBA {
+    return .{
+        .r = @intFromFloat(0.299 * @as(f32, @floatFromInt(gray))),
+        .g = @intFromFloat(0.587 * @as(f32, @floatFromInt(gray))),
+        .b = @intFromFloat(0.114 * @as(f32, @floatFromInt(gray))),
+        .a = 255,
+    };
+}
+
+fn toGRAY(
+    gpa: std.mem.Allocator,
+    data: []const u8,
+    data_order: DataOrder,
+) ![]GRAY {
+    var n_pixels: usize = undefined;
+    if (data_order == .gray) return gpa.dupe(GRAY, @as([]const GRAY, data));
+    const stride = try computeStride(data_order);
+    if (!isValidDataLen(data, stride)) return error.InvalidDataLen;
+    n_pixels = data.len / stride;
+    var new_data = try gpa.alloc(GRAY, n_pixels);
+    errdefer gpa.free(new_data);
+    const tag_order = try rgbaFromDataOrder(data_order);
+    var i: usize = 0;
+    switch (data_order) {
+        .gray => unreachable,
+        else => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = grayFromRGB(.{
+                    .r = data[i * stride + tag_order.r],
+                    .g = data[i * stride + tag_order.g],
+                    .b = data[i * stride + tag_order.b],
+                });
+            }
+        },
+        _ => return error.UnsupportedDataOrder,
+    }
+    return new_data;
+}
+
+fn toRGB(
+    gpa: std.mem.Allocator,
+    data: []const u8,
+    data_order: DataOrder,
+) ![]RGB {
+    var n_pixels: usize = undefined;
+    const stride = try computeStride(data_order);
+    if (!isValidDataLen(data, stride)) return error.InvalidDataLen;
+    n_pixels = data.len / stride;
+    var new_data = try gpa.alloc(RGB, n_pixels);
+    errdefer gpa.free(new_data);
+    const tag_order = try rgbaFromDataOrder(data_order);
+    var i: usize = 0;
+    switch (data_order) {
+        .gray => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = rgbFromGray(data[i]);
+            }
+        },
+        .rgb, .rbg, .grb, .gbr, .brg, .bgr, .rgba, .rbga, .grba, .gbra, .brga, .bgra => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = .{
+                    .r = data[i * stride + tag_order.r],
+                    .g = data[i * stride + tag_order.g],
+                    .b = data[i * stride + tag_order.b],
+                };
+            }
+        },
+        _ => return error.UnsupportedDataOrder,
+    }
+    return new_data;
+}
+
+fn toRGBA(
+    gpa: std.mem.Allocator,
+    data: []const u8,
+    data_order: DataOrder,
+) ![]RGBA {
+    var n_pixels: usize = undefined;
+    const stride = try computeStride(data_order);
+    if (!isValidDataLen(data, stride)) return error.InvalidDataLen;
+    n_pixels = data.len / stride;
+    var new_data = try gpa.alloc(RGBA, n_pixels);
+    errdefer gpa.free(new_data);
+    const tag_order = try rgbaFromDataOrder(data_order);
+    var i: usize = 0;
+    switch (data_order) {
+        .gray => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = rgbaFromGray(data[i]);
+            }
+        },
+        .rgb, .rbg, .grb, .gbr, .brg, .bgr => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = .{
+                    .r = data[i * stride + tag_order.r],
+                    .g = data[i * stride + tag_order.g],
+                    .b = data[i * stride + tag_order.b],
+                    .a = 255,
+                };
+            }
+        },
+        .rgba, .rbga, .grba, .gbra, .brga, .bgra => {
+            while (i < new_data.len) : (i += 1) {
+                new_data[i] = .{
+                    .r = data[i * stride + tag_order.r],
+                    .g = data[i * stride + tag_order.g],
+                    .b = data[i * stride + tag_order.b],
+                    .a = data[i * stride + tag_order.a],
+                };
+            }
+        },
+        _ => return error.UnsupportedDataOrder,
+    }
+    return new_data;
+}
+
 pub const Pixels = union(PixelOrder) {
     gray: []GRAY,
     rgb: []RGB,
@@ -48,75 +219,18 @@ pub const Pixels = union(PixelOrder) {
         data_order: DataOrder,
         pixel_order: PixelOrder,
     ) !@This() {
-        const j_step: usize = 1;
-        var i_step: usize = undefined;
-        switch (data_order) {
-            .gray => i_step = 1,
-            .rgb, .rbg, .grb, .gbr, .brg, .bgr => i_step = 3,
-            .rgba, .rbga, .grba, .gbra, .brga, .bgra => i_step = 4,
-            _ => return error.Unsupported,
-        }
-        if (@mod(data.len, i_step) != 0) //
-            return error.InvalidDimensions;
-        const new_data = switch (data_order) {
-            .gray => unreachable,
-            .rgb, .rgba, .rbg, .rbga, .grb, .gbr, .brg, .bgr, .grba, .gbra, .brga, .bgra => //
-            try gpa.alloc(RGB, data.len / i_step),
-            _ => unreachable,
+        return switch (pixel_order) {
+            .gray => .{ .gray = try toGRAY(gpa, data, data_order) },
+            .rgb => .{ .rgb = try toRGB(gpa, data, data_order) },
+            .rgba => .{ .rgba = try toRGBA(gpa, data, data_order) },
         };
-        errdefer gpa.free(new_data);
-        var r_step: usize = undefined;
-        var g_step: usize = undefined;
-        var b_step: usize = undefined;
-        var a_step: usize = undefined;
-        switch (data_order) {
-            .gray => unreachable,
-            .rgb, .rgba, .rbg, .rbga, .grb, .gbr, .brg, .bgr, .grba, .gbra, .brga, .bgra => |tag| {
-                r_step = std.mem.indexOfScalar(u8, @tagName(tag), 'r').?;
-                g_step = std.mem.indexOfScalar(u8, @tagName(tag), 'g').?;
-                b_step = std.mem.indexOfScalar(u8, @tagName(tag), 'b').?;
-                a_step = std.mem.indexOfScalar(u8, @tagName(tag), 'a') orelse 4;
-            },
-            _ => unreachable,
-        }
-        var i: usize = 0;
-        var j: usize = 0;
-        switch (pixel_order) {
-            .gray => {},
-            .rgb => {
-                while (i < data.len) : ({
-                    i += i_step;
-                    j += j_step;
-                }) {
-                    new_data[j] = .{
-                        .r = data[i + r_step],
-                        .g = data[i + g_step],
-                        .b = data[i + b_step],
-                    };
-                }
-            },
-            .rgba => {
-                while (i < data.len) : ({
-                    i += i_step;
-                    j += j_step;
-                }) {
-                    new_data[j] = .{
-                        .r = data[i + r_step],
-                        .g = data[i + g_step],
-                        .b = data[i + b_step],
-                        .a = data[i + a_step],
-                    };
-                }
-            },
-        }
-        return new_data;
     }
 
-    pub fn deinit(self: Pixels, gpa: std.mem.Allocator) void {
+    pub fn deinit(self: @This(), gpa: std.mem.Allocator) void {
         switch (self) {
-            .gray => |gray| gpa.free(gray),
-            .rgb => |rgb| gpa.free(rgb),
-            .rgba => |rgba| gpa.free(rgba),
+            .gray => |grays| gpa.free(grays),
+            .rgb => |rgbs| gpa.free(rgbs),
+            .rgba => |rgbas| gpa.free(rgbas),
         }
     }
 };
@@ -135,9 +249,9 @@ test "toPixel" {
         .{ .data_order = .bgr, .rgb = .{ .r = 1, .g = 100, .b = 255 } },
     };
     for (expected_rgbs) |expected_rgb| {
-        const rgb = try toRGB(gpa, rgb_data, expected_rgb.data_order);
-        defer gpa.free(rgb);
-        try std.testing.expectEqualDeep(expected_rgb.rgb, rgb[0]);
+        const rgb: Pixels = try .init(gpa, rgb_data, expected_rgb.data_order, .rgb);
+        defer rgb.deinit(gpa);
+        try std.testing.expectEqualDeep(expected_rgb.rgb, rgb.rgb[0]);
     }
 
     const rgba_data: []const u8 = &.{ 255, 100, 1, 255 };
@@ -150,12 +264,8 @@ test "toPixel" {
         .{ .data_order = .bgra, .rgb = .{ .r = 1, .g = 100, .b = 255 } },
     };
     for (expected_rgbas) |expected_rgba| {
-        const rgb = try toRGB(gpa, rgba_data, expected_rgba.data_order);
-        defer gpa.free(rgb);
-        try std.testing.expectEqualDeep(expected_rgba.rgb, rgb[0]);
+        const rgba: Pixels = try .init(gpa, rgba_data, expected_rgba.data_order, .rgba);
+        defer rgba.deinit(gpa);
+        try std.testing.expectEqualDeep(expected_rgba.rgba, rgba.rgba[0]);
     }
-}
-
-fn grayFromRGB(rgb: RGB) GRAY {
-    return @as(u8, @intFromFloat(0.299 * @as(f32, @floatFromInt(rgb.r)) + 0.587 * @as(f32, @floatFromInt(rgb.g)) + 0.114 * @as(f32, @floatFromInt(rgb.b))));
 }
